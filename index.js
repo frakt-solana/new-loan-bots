c90fea32632d8d7885c925d27c33cbcbe237f62a
davidberget@gmail.com <davidberget@gmail.com>
2022-06-06 11:35:31 -0500

    added endpoint

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
-dotenv.config()
-
 import {
   generateCardFile,
   removeCardFile,
@@ -12,14 +10,26 @@ import {
 import { getArweaveMetadataByMint } from './arweave/index.js'
 import { postTweet } from './twitter/index.js'
 import { initDiscord } from './discord/index.js'
+import { nftLending } from '@frakters/frakt-sdk'
+import { Connection, PublicKey } from '@solana/web3.js'
 
 const postOnDiscord = await initDiscord()
+
+dotenv.config()
+
+const NFT_LENDING_PROGRAM_ID = new PublicKey(
+  'A66HabVL3DzNzeJgcHYtRRNW1ZRMKwBfrdSR4kLsZ9DJ'
+)
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 const processedLoans = {
+  value: [],
+}
+
+const liquidationAlerts = {
   value: [],
 }
 
@@ -34,7 +44,7 @@ app.post('/new-loan', async (req, res) => {
     } = req.body
 
     if (processedLoans.value.includes(nftMint)) {
-      console.log(`This loan was already processed in last 10 min`)
+      console.log('This loan was already processed in last 10 min')
       return res.send('This loan was already processed in last 10 min')
     }
 
@@ -68,7 +78,7 @@ app.post('/new-loan', async (req, res) => {
     })
 
     if (!nftImageUrl || !nftName) {
-      console.log(`This nft has broken metadata`)
+      console.log('This nft has broken metadata')
       return res.send('This nft has broken metadata')
     }
 
@@ -97,6 +107,84 @@ app.post('/new-loan', async (req, res) => {
     removeCardFile(nftMint, processedLoans, 10 * 60 * 1000)
 
     res.send('Success')
+  } catch (error) {
+    console.error(error)
+    res.statusCode = 503
+    res.send('Oh shit!')
+  }
+})
+
+app.get('/liquidation-alert', async (req, res) => {
+  try {
+    const connection = new Connection(process.env.RPC_ENDPOINT, 'confirmed')
+
+    let { loans } = await nftLending.getters.getAllProgramAccounts({
+      programId: NFT_LENDING_PROGRAM_ID,
+      connection,
+    })
+
+    const nowSeconds = new Date().getTime() / 1000
+    const unfinishedLoans = loans.filter(
+      (loan) =>
+        loan.loanStatus === 'activated' &&
+        (loan.expiredAt - nowSeconds) / 60 / 60 < 24
+    )
+
+    const alerts = unfinishedLoans.map((loan) => {
+      if (liquidationAlerts.value.includes(loan.loanPublicKey)) {
+        console.log('This loan liquidation alert was already processed')
+      }
+
+      liquidationAlerts.value = [...liquidationAlerts.value, loan.loanPubkey]
+
+      const {
+        loanPubkey,
+        user,
+        nftMint,
+        nftUserTokenAccount,
+        liquidityPool,
+        collectionInfo,
+        startedAt,
+        expiredAt,
+        finishedAt,
+        originalPrice,
+        amountToGet,
+        rewardAmount,
+        feeAmount,
+        royaltyAmount,
+        rewardInterestRate,
+        feeInterestRate,
+        royaltyInterestRate,
+        loanStatus,
+        loanType,
+      } = loan
+
+      console.log('Loan data: ', loan)
+
+      // if (!nftImageUrl || !nftName) {
+      //   console.log(`This nft has broken metadata`)
+      //   return res.send('This nft has broken metadata')
+      // }
+
+      // const cardFilePath = generateCardFilePath(nftMint)
+
+      // await generateCardFile(nftMint, {
+      //   nftName,
+      //   nftImageUrl,
+      //   period,
+      //   loanToValue,
+      //   loanValue,
+      //   interest,
+      //   nftPrice,
+      // })
+
+      // await postOnDiscord(cardFilePath)
+      // removeCardFile(nftMint, processedLoans, 10 * 60 * 1000)
+
+      return user
+    })
+
+    res.send(`Successfully sent ${alerts.length} alerts`)
   } catch (error) {
     console.error(error)
     res.statusCode = 503
