26f848218ba412efbed54ddad63e76fe9725eac9
rustairov <rustairov@gmail.com>
2022-11-24 20:27:27 +0000

    New design

--- generateCard/index.js
+++ generateCard/index.js
@@ -14,9 +14,12 @@ const syneFontExtraBold = await font2base64.encodeToDataUrl(__dirname + '/fonts/
 const chakraRegular = await font2base64.encodeToDataUrl(__dirname + '/fonts/ChakraPetch/ChakraPetch-Regular.ttf');
 
 const logoImage = await readFile(__dirname + '/images/logo.svg', { encoding: 'base64' });
+const logoLightImage = await readFile(__dirname + '/images/logo_light.svg', { encoding: 'base64' });
 const backImage = await readFile(__dirname + '/images/back.svg', { encoding: 'base64' });
+const backLightImage = await readFile(__dirname + '/images/back_light.svg', { encoding: 'base64' });
 const newImage = await readFile(__dirname + '/images/new.svg', { encoding: 'base64' });
 const quoteImage = await readFile(__dirname + '/images/quote.svg', { encoding: 'base64' });
+const cornerImage = await readFile(__dirname + '/images/corner.svg', { encoding: 'base64' });
 const solanaImage = await readFile(__dirname + '/images/solana.svg', { encoding: 'base64' });
 
 export const generateCardFilePath = (id) => `${__dirname}/cards/card_${id}.png`;
@@ -255,13 +258,11 @@ const createLoanHTML = async ({ nftName, nftImageUrl, period, loanToValue, loanV
     
     .param-text-sol:after {
         position: absolute;
-        top: -2.5px;
+        top: -3px;
         content: url("data:image/svg+xml;base64, ${solanaImage}");
         width: 32px;
         height: 32px;
     }
-    
-    
   </style>
 </head>
 
@@ -323,123 +324,220 @@ const createRaffleHTML = async ({ nftName, nftImageUrl, buyoutPrice, floorPrice 
 <html>
 <head>
     <style>
-        @font-face {
-          font-family: "Syne-Bold";
-          src: url(${syneFontBold}) format('woff2');
-        }
-        @font-face {
-          font-family: "Syne-ExtraBold";
-          src: url(${syneFontExtraBold}) format('woff2');
-        }
-        @font-face {
-          font-family: "ChakraPetch-Regular";
-          src: url(${chakraRegular}) format('woff2');
-        }
+    @font-face {
+      font-family: "Syne-Bold";
+      src: url(${syneFontBold}) format('woff2');
+    }
+    @font-face {
+      font-family: "Syne-ExtraBold";
+      src: url(${syneFontExtraBold}) format('woff2');
+    }
+    @font-face {
+      font-family: "ChakraPetch-Regular";
+      src: url(${chakraRegular}) format('woff2');
+    }
 
-        * {
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-        }
+    * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
 
-        body {
-            color: #fff;
-            font-family: "Oxanium-regular";
-            font-size: 16px;
-            width: 1200px;
-            height: 675px;
-        }
+    body {
+      color: #000000;
+      font-family: ChakraPetch-Regular;
+      font-size: 16px;
+      width: 1024px;
+      height: 512px;
+    }
 
-        .wrapper {
-            width: 1200px;
-            height: 675px;
-            background-color: #5D5FEF;
-            color: white;
-        }
+    .wrapper {
+      width: 1024px;
+      height: 512px;
+      background-color: #F2F2F7;
+      background-image: url("data:image/svg+xml;base64, ${backLightImage}");
+      background-repeat: no-repeat;
+      background-size: 225px 200px;
+      background-position: right 0 top 0;
+    }
+    
+    .image {
+        width: 512px;
+        height: 512px;
+        background-image: url('${base64}');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    
+    .card {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+    }
+    
+    .frakt-logo {
+      position: absolute;
+      width: 80px;
+      height: 24px;
+      left: 532px;
+      top: 468px;
+    }
+    
+    .quote-top-left {
+      position: absolute;
+      width: 103px;
+      height: 76px;
+      left: 532px;
+      top: 20px;
+    }
+    
+    .quote-bottom-right {
+      position: absolute;
+      width: 103px;
+      height: 76px;
+      left: 910px;
+      top: 416px;
+      transform: rotate(-180deg);
+    }
+    
+    .quote-top-right {
+      position: absolute;
+      width: 103px;
+      height: 76px;
+      left: 910px;
+      top: 20px;
+      transform: matrix(-1, 0, 0, 1, 0, 0);
+    }
+    
+    .new {
+      position: absolute;
+      width: 115px;
+      height: 42px;
+      left: 710px;
+      top: 36px;
+    }
+    
+    .info {
+      width: 512px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      height: 100%;
+    }
 
-        .card {
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-            display: flex;
-            align-items: center;
-        }
+    .title {
+      z-index: 1;
+      width: 100%;
+      font-family: Syne-ExtraBold;
+      text-transform: uppercase;
+      text-align: center;
+      font-size: 40px;
+      line-height: 80%;
+      margin-top: 64px;
+      margin-bottom: 20px;
+    }
+    
+    .name {
+      width: 100%;
+      font-family: Syne-Bold;
+      font-size: 32px;
+      line-height: 38px;
+      text-align: center;
+      text-transform: uppercase;
+    }
 
-        .image {
-            width: 675px;
-            height: 675px;
-            background-image: url('${base64}');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }
+    .data {
+        width: 512px;
+        position: absolute;
+        top: 268px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 30px;
+    }
+    
+    .row {
+        display: flex;
+        justify-content: space-around;
+    }
 
-        .info {
-            flex: 1 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .frakt-logo {
-            height: 30px;
-            margin-bottom: 30px;
-        }
-
-        .title {
-            font-family: "Oxanium-extra-bold";
-            text-transform: uppercase;
-            text-align: center;
-            font-size: 92px;
-            line-height: 80%;
-            margin-bottom: 30px;
-        }
-        
-        .name {
-            font-family: "Oxanium-extra-bold";
-            text-transform: capitalize;
-            font-size: 36px;
-            line-height: 45px;
-            margin-bottom: 30px;        
-        }
-
-        .text {
-            width: 100%;
-            font-family: "Oxanium-medium";
-            font-size: 36px;
-            line-height: 45px;
-            text-transform: capitalize;
-        }
-
-        .bold {
-            font-family: "Oxanium-extra-bold";
-            text-transform: uppercase;
-        }
-
-        .flex {
-            padding: 0 20px;
-            display: flex;
-            justify-content: space-between;
-        }
-
-
-    </style>
+    .param {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        row-gap: 16px;
+    }
+    
+    .param-title {
+        margin: 0;
+        color: #AEAEB2;
+        font-family: Syne-Bold;
+        font-size: 25px;
+        line-height: 80%;
+        text-transform: uppercase;
+    }
+    
+    .param-text {
+        margin: 0;
+        font-family: ChakraPetch-Regular;
+        font-size: 42px;
+        line-height: 80%;
+        text-transform: uppercase;
+    }
+    
+    .param-text-sol {
+        position: relative;
+        margin: 0;
+        font-family: ChakraPetch-Regular;
+        font-size: 42px;
+        line-height: 80%;
+        text-transform: uppercase;
+    }
+    
+    .param-text-sol:after {
+        position: absolute;
+        top: 0;
+        content: url("data:image/svg+xml;base64, ${solanaImage}");
+        width: 52px;
+        height: 52px;
+    }
+  </style>
 </head>
 
 <body>
 <div class="wrapper">
     <div class="card">
-        <div class="image"></div>
-        <div class="info">
-            <img src="data:image/svg+xml;base64, ${logoImage}" class="frakt-logo" />
-            <h1 class="title">New raffle</h1>
-            <p class="name">${nftName}</p>
-            <p class="text flex">Buyout price: <span class="bold">${buyoutPrice} SOL</span></p>
-            <p class="text flex">Floor price: <span class="bold">${floorPrice} SOL</span></p>
+      <div class="image"></div>
+      <div class="info">
+        <img src="data:image/svg+xml;base64, ${logoLightImage}" class="frakt-logo" />
+        <img src="data:image/svg+xml;base64, ${cornerImage}" class="quote-top-left" />
+        <img src="data:image/svg+xml;base64, ${cornerImage}" class="quote-top-right" />
+        <img src="data:image/svg+xml;base64, ${cornerImage}" class="quote-bottom-right" />
+        <img src="data:image/svg+xml;base64, ${newImage}" class="new" />
+        
+        <h1 class="title">Raffle</h1>
+        <h2 class="name">${nftName}</h2>
+        <div class="data">
+            <div class="row">
+                <div class="param">
+                    <p class="param-title">Floor price</p>
+                    <p class="param-text-sol">${floorPrice}</p>
+                </div>            
+            </div>
+            <div class="row">
+                <div class="param">
+                    <p class="param-title">Buyout price</p>
+                    <p class="param-text-sol">${buyoutPrice}</p>
+                </div>
+            </div>           
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </body>
 </html>
 `
